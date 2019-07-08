d78eff2e08397212aec6eebf1b9fbd440fc7e120
Asaye Dilbo <32922135+Asaye@users.noreply.github.com>
2019-07-08 14:04:23 +0300

    Add files via upload

---
+++ styles/Colors.js
@@ -0,0 +1,52 @@
+const Colors_default = {	
+	"datepicker_header": "#222",
+	"datepicker_txt": "#ddd",
+	"datepicker_txt_active": "#ddd",	
+	"datepicker_txt_adj": "#ccc",
+	"datepicker_back": "#777",
+	"datepicker_current": "#555",	
+	"datepicker_adj": "#888",
+	"datepicker_active": "#000",
+	"datepicker_border": "#555",
+};
+
+const Colors_greenish = {
+	"datepicker_header": "#007E33",
+	"datepicker_txt": "#eee",
+	"datepicker_txt_active": "#ddd",	
+	"datepicker_txt_adj": "#ddd",
+	"datepicker_back": "#777",
+	"datepicker_current": "#66bb6a",	
+	"datepicker_adj": "#9ccc65",
+	"datepicker_active": "#1b5e20",
+	"datepicker_border": "#555",
+};
+
+const Colors_bluish = {
+	"datepicker_header": "#1565c0",
+	"datepicker_txt": "#eee",
+	"datepicker_txt_active": "#ddd",	
+	"datepicker_txt_adj": "#ddd",
+	"datepicker_back": "#777",
+	"datepicker_current": "#2196f3",	
+	"datepicker_adj": "#90caf9",
+	"datepicker_active": "#1a237e",
+	"datepicker_border": "#555",
+};
+
+const Colors_reddish = {	
+	"datepicker_header": "#c62828",
+	"datepicker_txt": "#eee",
+	"datepicker_txt_active": "#ddd",	
+	"datepicker_txt_adj": "#ddd",
+	"datepicker_back": "#777",
+	"datepicker_current": "#ef5350",	
+	"datepicker_adj": "#ef9a9a",
+	"datepicker_active": "#b71c1c",
+	"datepicker_border": "#555",
+};
+
+module.exports.Colors_default = Colors_default;
+module.exports.Colors_greenish = Colors_greenish;
+module.exports.Colors_bluish = Colors_bluish;
+module.exports.Colors_reddish = Colors_reddish;
